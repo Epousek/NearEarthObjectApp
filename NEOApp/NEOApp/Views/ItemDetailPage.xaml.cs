964ec8fb4e26c0b1675347663d23c0d96e45bd5d
Epousek <janbezouska@outlook.com>
2021-12-20 09:35:02 +0100

    create project

---
+++ NEOApp/NEOApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using NEOApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace NEOApp.Views
+{
+  public partial class ItemDetailPage : ContentPage
+  {
+    public ItemDetailPage()
+    {
+      InitializeComponent();
+      BindingContext = new ItemDetailViewModel();
+    }
+  }
+}
